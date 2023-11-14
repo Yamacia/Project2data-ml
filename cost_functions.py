3b504d286bdcc8c2e1f27b1757863c1c598b5bc1
Sofriet <slmvos@kpnmail.nl>
2023-11-14 19:17:40 +0100

    Cost function to compute R2 score

--- cost_functions.py
+++ cost_functions.py
@@ -25,3 +25,16 @@ def CostCrossEntropy(target):
         return -(1.0 / target.size) * np.sum(target * np.log(X + 10e-10))
 
     return func
+
+def CostR2score(target):
+    
+    def func(X):
+        ss_res = np.sum((target - X) ** 2)
+        ss_tot = np.sum((target - np.mean(target)) ** 2)
+        #note we dont do 1 - (ss_res / ss_tot), such that the best fit is when r2_score returns 0
+        r2_score = ss_res / ss_tot
+        return r2_score
+    return func
+
+
+
