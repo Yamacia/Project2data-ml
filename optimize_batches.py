edcbaff4786c99be22c22b52a1b0075b6c9145dc
kristianbugge <127785444+kristianbugge@users.noreply.github.com>
2023-11-14 12:09:53 +0100

    Add files via upload

--- optimize_batches.py
+++ optimize_batches.py
@@ -22,10 +22,21 @@ epochs = 500
 #Number of folds for cross validation
 folds = 5
 #Generates either Skranke or Franke dataset
-x, y, z, X, X_train, X_test, z_train, z_test = generate_synth_dataset(use_franke, noise, 1 / datapoints, maxDegree)
-ffnn = FFNN((X.shape[1], 1), seed = seed)
-
+x, y, z, X, X_train, X_test, z_train, z_test = generate_dataset(use_franke, noise, 1 / datapoints, maxDegree)
+ffnn = FFNN((X.shape[1], int(X.shape[1] + 1 / 2), 1), seed = seed)
+lam = 0.001
 rho = 0.9
 rho2 = 0.99
 momentum = 0.5
-batches = 32
+batches_list = [8, 16, 32, 64, 128]
+adam = Adam(eta = 0.01, rho=rho, rho2=rho2)
+for batch in batches_list:
+    scores = ffnn.cross_validation(X_train, z_train, folds, adam, batch, epochs, lam)
+    lab = f"Batches: {batch}"
+    plt.plot(scores["val_errors"], label = lab)
+
+plt.title("MSE as a function of epochs")
+plt.legend()
+plt.xlabel("Epochs")
+plt.ylabel("MSE")
+plt.show()
